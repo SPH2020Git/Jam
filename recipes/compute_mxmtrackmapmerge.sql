ae155113a6864e9e61f381cf47a304c3e4834172
admin <admin>
2017-08-11 15:20:32 -0400

    Saved recipe: JAM.compute_mxmtrackmapmerge

--- recipes/compute_mxmtrackmapmerge.sql
+++ recipes/compute_mxmtrackmapmerge.sql
@@ -1,3 +1,3 @@
-SELECT distinct "artist_simplified","song_simplified","mxm_mxm_trackid"
+SELECT distinct "artist_simplified","song_simplified"
     FROM "JAM_mxmtrackidmap"
     where mxm_mxm_trackid is null
