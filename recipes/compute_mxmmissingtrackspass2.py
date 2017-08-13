44133a75792d071821a332e3660794112e818d38
admin <admin>
2017-08-13 16:18:00 -0400

    Saved recipe: JAM.compute_mxmmissingtrackspass2

--- recipes/compute_mxmmissingtrackspass2.py
+++ recipes/compute_mxmmissingtrackspass2.py
@@ -24,17 +24,33 @@ songslist=missingtracks['song_simplified'].tolist()
 thekey="75a92c53aece5428a5e239011843a4fd"
 thetracks=pd.DataFrame()
 for idx, val in enumerate(songslist):
-        if idx<1001:
+        if idx<2001:
             theartist='"'+artistslist[idx]+'"'
             thetitle='"'+songslist[idx]+'"'
             print(theartist+thetitle)
             response=requests.get("https://api.musixmatch.com/ws/1.1/matcher.track.get?format=jsonp&callback=callback&q_artist="+theartist+"&q_track="+thetitle+"&apikey="+thekey).text
             responseclean = response.split("(")[1].strip(");")
             if responseclean[-1]!="}":
+#                 responseclean=ast.literal_eval(responseclean+'"}}}}') 
+#                 parsed_json=pd.io.json.json_normalize(responseclean)
+#                 thetracks=thetracks.append(parsed_json)
                 continue
             data_json = json.loads(responseclean)
             parsed_json=pd.io.json.json_normalize(data_json)
             thetracks=thetracks.append(parsed_json)
+ 
+# convert id fields to int, otherwise they show up in scientific notation
+cols=[
+"message.body.track.album_id",\
+"message.body.track.artist_id",\
+"message.body.track.commontrack_id",\
+"message.body.track.lyrics_id",\
+"message.body.track.subtitle_id",\
+"message.body.track.track_id",\
+    ]
+
+thetracks[cols] = thetracks[cols].fillna(0.0).astype(int)
+
 
 # Recipe outputs
 mxmmissingtrackspass2 = dataiku.Dataset("mxmmissingtrackspass2")
